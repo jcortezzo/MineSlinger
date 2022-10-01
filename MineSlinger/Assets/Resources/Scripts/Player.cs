6726c8d4295ec4f3df222723a5882e1e62bd248d
Jonah <34112011+jcortezzo@users.noreply.github.com>
2022-09-30 21:29:27 -0700

    Add movement logic

--- MineSlinger/Assets/Resources/Scripts/Player.cs
+++ MineSlinger/Assets/Resources/Scripts/Player.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     [SerializeField]
+    private float moveSpeed;
+    [SerializeField]
     private float slideForce;
     [SerializeField]
     private float slideCd;
@@ -24,7 +26,7 @@ public class Player : MonoBehaviour
         }
     }
     private Vector2 lastDirectionWalked;
-    private float minSlidingSpeed = 1f;
+    private float minSlidingSpeed = 2f;
 
     private Rigidbody2D rb;
     private Animator anim;
@@ -44,6 +46,17 @@ public class Player : MonoBehaviour
         {
             Slide();
         }
+        Move();
+    }
+
+    public void Move()
+    {
+        if (!CanSlide) return;  // TODO: Change to isSliding
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+        Vector2 moveDir = new Vector2(horizontal, vertical).normalized;
+        rb.velocity = moveDir * moveSpeed;
+        anim.SetBool("IsWalking", rb.velocity.magnitude >= minSlidingSpeed);
     }
 
     public void Slide()
@@ -58,12 +71,13 @@ public class Player : MonoBehaviour
     {
         CanSlide = false;
         anim.SetBool("IsSliding", true);
+        anim.SetBool("IsWalking", false);
         // might not be necessary to have a cooldown if we judge
         // just off the player's speed... we'll see, maybe we can remove this
-        for (float t = 0; t < slideCd; t += Time.deltaTime)
-        {
-            yield return new WaitForEndOfFrame();
-        }
+        //for (float t = 0; t < slideCd; t += Time.deltaTime)
+        //{
+        //    yield return new WaitForEndOfFrame();
+        //}
         while (rb.velocity.magnitude > minSlidingSpeed)
         {
             yield return new WaitForEndOfFrame();
